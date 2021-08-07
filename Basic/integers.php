af58784dd2ca217ef111e5c976baabb66ff85635
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-08-07 13:48:21 +0530

    Create integers.php

---
+++ Basic/integers.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Integers</title>
+</head>
+<body>
+    <?php
+        $var1=30;
+        $var2=40;
+        echo $var1+$var2;
+        
+    ?>
+    <br>
+    Square root:<?php echo sqrt(100)?></body>
+</html>
