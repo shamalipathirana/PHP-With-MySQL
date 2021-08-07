21ca1db5038f15434b1147c4e2f0eb79986335f5
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-08-07 11:53:44 +0530

    Create string-functions.php

---
+++ Basic/string-functions.php
@@ -0,0 +1,16 @@
+<?php
+
+$first="I am shamali";
+$second=" I am an undergraduate";
+$sentence=$first;
+$sentence .= $second;
+echo $sentence;
+
+?>
+
+<br>
+
+
+<?php
+echo strtolower($sentence);
+?>
