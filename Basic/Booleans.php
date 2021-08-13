83103ecf447fd3f06d87670600daf96c6282bc71
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-08-14 04:43:45 +0530

    Create Booleans.php

---
+++ Basic/Booleans.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Booleans</title>
+</head>
+<body>
+    <?php
+    $var1=true;
+    $var2=false;
+    echo $var1;
+    echo "<br>";
+    echo $var2;
+    ?>
+</body>
+</html>
