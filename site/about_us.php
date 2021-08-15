0f1e65133582230655b479b770b3bc1dc18c4226
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-08-15 14:21:23 +0530

    Update about_us.php

--- site/about_us.php
+++ site/about_us.php
@@ -1,24 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Website</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <header>
-        <h1>Website Name</h1>
-    </header>
-    <nav>
-    <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="about_us.php">About</a></li>
-        <li><a href="contact_us.php">Contact</a></li>
-        <li><a href="products">Products</a></li>
-    </ul>
-    </nav>
+<?php
+include('includes/header.php');
+?>
     <main>
     <article>
         <h3>About Us</h3>
@@ -28,8 +10,7 @@
     </article>
     </main>
 
-    <footer>
-        <p>Company name | Address | Tel:0115678963</p>
-    </footer>
-</body>
-</html>
+    <?php
+include('includes/footer.php');
+
+?>
