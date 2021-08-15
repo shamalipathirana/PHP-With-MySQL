12271289e2f9653c708eba8239a83db2ed081f2c
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-08-15 19:36:59 +0530

    Create login.php

---
+++ site/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In</title>
+</head>
+<body>
+    <form action="process-login.php" method="post">
+        username:<input type="text" name="username" id=""><br>
+        password:<input typr="password" name="password" id=""><br>
+        <input type= "submit" value="Log In" name="submit">
+    </form>
+</body>
+</html>
