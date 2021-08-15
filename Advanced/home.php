1fa4c83b11ee6884bf297941e2c4c65186454d37
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-08-15 11:51:42 +0530

    Create home.php

---
+++ Advanced/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h1>Home Page</h1>
+    <?php
+    $news_items=312;
+    $category="sports";
+    $url=
+    ?>
+    <a href="news.php?news_items=32&category=sports">Read News Items</a>
+</body>
+</html>
