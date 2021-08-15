0579c758516f54d0566f3331a91f1f197c48adbf
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-08-15 13:02:05 +0530

    Update index.php

--- site/index.php
+++ site/index.php
@@ -5,8 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Website</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    
+    <header>
+        <h1>Website Name</h1>
+    </header>
+    <nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about_us.php">About</a></li>
+        <li><a href="contact_us.php">Contact</a></li>
+        <li><a href="products">Products</a></li>
+    </ul>
+    </nav>
+
 </body>
 </html>
