35bb330822c693f898372e63baf39cea835eb620
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-08-15 13:36:30 +0530

    Update about_us.php

---
+++ site/about_us.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <h1>Website Name</h1>
+    </header>
+    <nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about_us.php">About</a></li>
+        <li><a href="contact_us.php">Contact</a></li>
+        <li><a href="products">Products</a></li>
+    </ul>
+    </nav>
+    <main>
+    <article>
+        <h3>About Us</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati itaque, numquam repellat, 
+            hic molestiae fugiat nobis nesciunt eius ratione delectus porro fuga molestias aspernatur libero 
+            aliquam! Rem impedit sapiente quo.</p>
+    </article>
+    </main>
+
+    <footer>
+        <p>Company name | Address | Tel:0115678963</p>
+    </footer>
+</body>
+</html>
