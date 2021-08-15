49564e339817ff8bf7bb9a98662e201b934a1454
shamalipathirana <shyamalee.123.pathirana@gmail.com>
2021-08-15 13:41:11 +0530

    Update index.php

--- site/index.php
+++ site/index.php
@@ -1,24 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Website</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <header>
-        <h1>Website Name</h1>
-    </header>
-    <nav>
-    <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="about_us.php">About</a></li>
-        <li><a href="contact_us.php">Contact</a></li>
-        <li><a href="products">Products</a></li>
-    </ul>
-    </nav>
+<?php
+include('includes/header.php');
+?>
     <main>
     <article>
         <h3>This is home page</h3>
@@ -27,9 +9,7 @@
             aliquam! Rem impedit sapiente quo.</p>
     </article>
     </main>
+<?php
+include('includes/footer.php');
 
-    <footer>
-        <p>Company name | Address | Tel:0115678963</p>
-    </footer>
-</body>
-</html>
+?>
